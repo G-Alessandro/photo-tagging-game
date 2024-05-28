8f4e13408ad3be23e9b59673f392ef209f33388c
G-Alessandro <galloalessandro77@gmail.com>
2024-05-28 12:57:30 +0200

    Added cors and mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,27 @@
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 
+const mongoDB = process.env.MONGO_URI;
+const allowedOrigin = process.env.ALLOWED_ORIGIN || '';
+
 const app = express();
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+app.use(cors({
+  origin: allowedOrigin,
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,5 +30,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.json();
+});
 
 module.exports = app;
